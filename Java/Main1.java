8e0c8c32a366cfef89c7a46c09dd0847baae2701
aarushpandey772 <pandeyaarush91@gmail.com>
2026-08-20 07:25:19 +0200

    Add files via upload

---
+++ Java/Main1.java
@@ -0,0 +1,11 @@
+import library.Book;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+
+        Book b = new Book(101, "Java Programming", "Herbert Schildt", 699);
+
+        b.display();
+    }
+}
